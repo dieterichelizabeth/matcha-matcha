e281d56527c026c6fd036e4458908e7eec9895d5
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-06-25 18:02:55 -0500

    Adds intial content to Homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import HomeCarousel from "../components/HomeCarousel";
+import HomeCards from "../components/HomeCards";
 
 const Home = () => {
   return (
     <div>
-      <p>hello world</p>
+      <p className="home-description">
+        Clinically Tested, Ethically Sourced Skincare{" "}
+      </p>
+      <h1 className="home-company-name">Matcha Matcha Skincare</h1>
+      <HomeCarousel />
+      <HomeCards />
     </div>
   );
 };
