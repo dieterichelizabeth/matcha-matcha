85e264707e5afdf4c5318b352dbfd6491f4dc5ea
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-06-29 10:09:14 -0500

    Adds initial success page

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Button, Result } from "antd";
+import { Link } from "react-router-dom";
 
 const Success = () => {
+  const orderNumber = "2017182818828182881";
   return (
     <div>
-      <p>hello world</p>
+      <Result
+        status="success"
+        title="Success!"
+        subTitle={`Thank you for your purchase! Order number: ${orderNumber}`}
+        extra={[
+          <Button type="primary" key="console">
+            <Link to="/">Back Home</Link>
+          </Button>,
+          <Button key="buy">
+            <Link to="/orderHistory">View Your Orders</Link>
+          </Button>,
+        ]}
+      />
     </div>
   );
 };
