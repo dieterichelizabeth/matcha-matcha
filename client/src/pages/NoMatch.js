5feb274479e2ed28576abc68a148b60a6c47cd89
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-06-29 10:01:37 -0500

    Adds noMatch page from Ant Design

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Button, Result } from "antd";
+import { Link } from "react-router-dom";
 
 const NoMatch = () => {
   return (
     <div>
-      <p>hello world</p>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary">
+            <Link to="/">Back Home</Link>
+          </Button>
+        }
+      />
     </div>
   );
 };
