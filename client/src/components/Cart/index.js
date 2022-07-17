335068b4c416dd8699997c650e7a1d03be38a0a2
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-07-17 17:38:10 -0500

    Add the Cart UI

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import "./style.css";
 
 const Cart = () => {
   // React-Redux dispatch hook for adding products to the Redux store.
@@ -14,7 +15,7 @@ const Cart = () => {
 
   if (!store.cartOpen) {
     return (
-      <div className="shopping-cart" onClick={toggleCart}>
+      <div className="cart" onClick={toggleCart}>
         <p>
           <span className="material-symbols-outlined">shopping_bag</span>
         </p>
@@ -23,9 +24,22 @@ const Cart = () => {
   }
 
   return (
-    <div className="shopping-cart">
-      <div onClick={toggleCart}>[close]</div>
-      <p>Open</p>
+    <div className="shopping-cart-open">
+      <div className="shopping-cart-close-button" onClick={toggleCart}>
+        [close]
+      </div>
+      <div className="cart-contents">
+        <p>Nothing added to cart yet!</p>
+      </div>
+
+      <hr></hr>
+      <div className="cart-open-amount-container">
+        <p className="cart-open-total">CART SUBTOTAL: </p>
+        <p>$170.00</p>
+      </div>
+      <hr></hr>
+      <button className="view-cart-button">VIEW CART</button>
+      <button className="checkout-button">CHECKOUT</button>
     </div>
   );
 };
