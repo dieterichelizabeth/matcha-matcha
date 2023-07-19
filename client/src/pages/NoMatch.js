a1b91d3a1f7c509e4d21c947467b45e8af4b4a10
Elizabeth Dieterich <dieterichelizabeth@gmail.com>
2023-07-19 00:21:13 -0500

    404 page updates

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,19 +1,37 @@
 import React from "react";
+import Notification from "../components/Notification";
+import Nav from "../components/Nav";
 import Footer from "../components/Footer";
+import NotificationSupport from "../components/NotificationSupport";
+import "./nomatch.css";
 
 const NoMatch = () => {
-  // Redirect to home-page in 10 secconds
-  setTimeout(() => {
-    window.location.assign("/");
-  }, 5000);
-
   return (
-    <div>
+    <>
+      <Notification />
+      <Nav />
+      <hr></hr>
+
       <div className="no-match-page">
-        <div className="no-match-graphic"></div>
+        <div className="no-match-page__img-container">
+          <img
+            src={require("../assets/404-not-found.png")}
+            alt="A teacup sitting on top of a saucer sitting on a leaf with the word 404 written."
+          ></img>
+        </div>
+        <div className="no-match-page__text-container">
+          <h2>Error 404: Page Not Found</h2>
+          <p>
+            We are unable to find the page requested. It may not exist, or has
+            been deleted.
+          </p>
+          <a href="/">Visit the Homepage</a>
+        </div>
       </div>
+
+      <NotificationSupport />
       <Footer />
-    </div>
+    </>
   );
 };
 
